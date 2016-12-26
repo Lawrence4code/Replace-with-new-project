ea931f99f827e4a4c7be41670d77958dbc087b49
Lawrence <Lawrence4code@gmail.com>
2016-12-26 23:30:31 +0530

    Completed till site footer

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,25 +2,26 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create();
 
-gulp.task('watch', function() {
-
+gulp.task('watch', function () {
+    'use strict';
     browserSync.init({
-        notify: false,
         server: {
             baseDir: "app"
         }
     });
-    
-  watch('./app/index.html', function() {
-    browserSync.reload();
-  });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
-  });
+    watch('./app/index.html', function () {
+        browserSync.reload('./app/index.html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function () {
+        gulp.start('cssInject');
+    });
+
 });
 
-gulp.task('cssInject', ['styles'], function(){
-   return gulp.src('./app/temp/styles/styles.css')
-   .pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function () {
+    'use strict';
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
 });
