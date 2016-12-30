de1108dc283add9a6ed75338479e8048e9b70567
Lawrence <Lawrence4code@gmail.com>
2016-12-30 22:05:44 +0530

    Completed destop and mobile header style.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,6 +17,10 @@ gulp.task('watch', function () {
     watch('./app/assets/styles/**/*.css', function () {
         gulp.start('cssInject');
     });
+    
+    watch('./app/assets/scripts/**/*.js', function () {
+        gulp.start('scriptsRefresh');
+    });
 
 });
 
@@ -24,4 +28,9 @@ gulp.task('cssInject', ['styles'], function () {
     'use strict';
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function () {
+    'use strict';
+    browserSync.reload();
 });
